01847f275646fa2a1c76012b951c4fc1aed31483
stairman06 <ransom@wheeler.me>
2019-01-24 20:52:54 -0600

    Back button, serach bar, header buttons

---
+++ src/component/button/button.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+const Button = styled.div`
+    padding: 11.5px;
+    color: white;
+    width: fit-content;
+    display: inline-block;
+    ${props => props.color === 'purple' && `
+        background-color: #911895;
+    `}
+    ${props => props.color === 'green' && `
+        background-color: #15950C;
+    `}
+    cursor: pointer;
+    transition: 300ms;
+    &:hover {
+        filter: brightness(0.75);
+    }
+`
+
+export default Button;
