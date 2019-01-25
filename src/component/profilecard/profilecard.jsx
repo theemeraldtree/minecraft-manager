c521b05b40bf6225e14f760df6fb44b07ab0d15b
stairman06 <ransom@wheeler.me>
2019-01-25 16:57:49 -0600

    Profile card example

---
+++ src/component/profilecard/profilecard.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from './logo.png';
+import Button from '../button/button';
+const BG = styled.div`
+    width:120px;
+    height: 190px;
+    background-color: #717171;
+    display: inline-flex;
+    margin: 5px;
+    align-items: center;
+    flex-flow: column;
+    cursor: pointer;
+    user-select: none;
+    transition: 150ms;
+    position: relative;
+    &:hover {
+        background-color: #5b5b5b;
+    }
+`
+
+const Image = styled.div`
+    background-image: url('${logo}');
+    width: 100%;
+    height: 80px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin-top: 5px;
+    background-position: center;
+`
+
+const Title = styled.p`
+    color: white;
+    font-weight: bolder;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: center;
+    margin: 5px 2px;
+    user-select: none;
+`
+
+const Buttons = styled.div`
+    display: flex;
+    flex-flow: row;
+    bottom: 7px;
+    position: absolute;
+`
+
+const LaunchButton = styled(Button)`
+    padding-top: 6px;
+    padding-bottom: 6px;
+`
+
+const EditButton = styled(Button)`
+    padding: 6px 0 6px 0;
+    width: 31px;
+    margin-left: 5px;
+`
+
+const ProfileCard = () => (
+    <BG>
+        <Image />
+        <Title>Minecraft Manager is great!!</Title>
+        <Buttons>
+            <LaunchButton color='green'>launch</LaunchButton>
+            <EditButton color='yellow' />
+        </Buttons>
+    </BG>
+)
+
+export default ProfileCard;
--- src/page/home/homePage.jsx
+++ src/page/home/homePage.jsx
@@ -3,13 +3,49 @@ import Page from '../page';
 import Header from '../../component/header/header';
 import SearchBox from '../../component/searchbox/searchbox';
 import Button from '../../component/button/button';
+import ProfileCard from '../../component/profilecard/profilecard';
+import styled from 'styled-components';
+
+const CardGrid = styled.div`
+    overflow-y: scroll;
+    flex: 1 1 auto;
+    padding-bottom: 10px;
+`;
+
 const HomePage = () => (
     <Page>
-        <Header title='profiles' backlink='/'>
+        <Header title='profiles'>
             <SearchBox placeholder='search' />
             <Button color='purple'>import</Button>
             <Button color='green'>create</Button>
         </Header>
+        <CardGrid>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </CardGrid>
     </Page>
 )
 
