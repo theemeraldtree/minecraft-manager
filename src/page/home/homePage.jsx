083cf27d950f1ee10e481a73e945b08fd868dc57
stairman06 <ransom@wheeler.me>
2019-01-26 21:49:26 -0600

    Search function

--- src/page/home/homePage.jsx
+++ src/page/home/homePage.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Page from '../page';
 import Header from '../../component/header/header';
 import SearchBox from '../../component/searchbox/searchbox';
 import Button from '../../component/button/button';
 import ProfileGrid from './components/profilegrid';
 
-const HomePage = () => (
-    <Page>
-        <Header title='profiles'>
-            <SearchBox placeholder='search' />
-            <Button color='purple'>import</Button>
-            <Button color='green'>create</Button>
-        </Header>
-        <ProfileGrid />
-    </Page>
-)
+export default class HomePage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchTerm: ''
+        }
+    }
 
-export default HomePage;
+    searchChange = (e) => {
+        this.setState({
+            searchTerm: e.target.value.toLowerCase()
+        })
+    }
+    
+    render() {
+        return (
+            <Page>
+                <Header title='profiles'>
+                    <SearchBox onChange={this.searchChange} placeholder='search' />
+                    <Button color='purple'>import</Button>
+                    <Button color='green'>create</Button>
+                </Header>
+                <ProfileGrid searchTerm={this.state.searchTerm} />
+            </Page>
+        )
+    }
+
+}
