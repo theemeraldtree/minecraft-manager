05db388dd16dce34b1889eb37dacee9be6b9901b
stairman06 <36215135+stairman06@users.noreply.github.com>
2019-12-07 17:11:31 -0600

    update wiki link

--- src/page/settings/pages/about.jsx
+++ src/page/settings/pages/about.jsx
@@ -102,7 +102,7 @@ export default function About() {
                     <Logo />
                     <div>
                         <h1>About Minecraft Manager</h1>
-                        <h3>Version 2.1a [Alpha Branch]</h3>
+                        <h3>Version 2.1.0-b1 [Beta Branch]</h3>
                         <h3>released 12/7/2019</h3>
                                        
                         <Updates>
